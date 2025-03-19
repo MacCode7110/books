5beeba373a7fe8d4f974173581e8bb5ecde9da17
MacCode7110 <mattmcal88@gmail.com>
2025-03-19 17:01:26 -0400

    Add Provider component to books.js

--- src/context/books.js
+++ src/context/books.js
@@ -1,5 +1,23 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
 
 const BooksContext = createContext();
 
+function Provider({ children }) {
+    const [count, setCount] = useState(5);
+
+    const valueToShare = {
+        count,
+        incrementCount: () => {
+            setCount(count + 1);
+        }
+    }
+
+    return <BooksContext.Provider value={valueToShare}>
+        {children}
+    </BooksContext.Provider>
+}
+
+// We always need a named export for the Provider component
+export { Provider };
+// We always need a default export for the Context object
 export default BooksContext;
