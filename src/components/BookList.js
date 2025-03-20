5caebec4e306c551a2df33846efcf655fefd76af
MacCode7110 <mattmcal88@gmail.com>
2025-03-20 13:11:44 -0400

    Add count piece of state to BookList and increment count on click of the button

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -3,13 +3,16 @@ import BooksContext from '../context/books'
 import BookShow from './BookShow';
 
 function BookList({ books, onDelete, onEdit }) {
-    const value = useContext(BooksContext);
+    // Use object destructuring to access count and incrementCount
+    const { count, incrementCount } = useContext(BooksContext);
     const renderedBooks = books.map((book) => {
         return (<BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book}/>);
     });
 
+    // NOTE: We can include multiple different variables with each one surrounded by curly brackets in the JSX that is rendered
     return <div className="book-list">
-        {value}
+        {count}
+        <button onClick={incrementCount}>Click</button>
         {renderedBooks}</div>;
 }
 
